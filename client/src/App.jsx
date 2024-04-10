67b0660a5f458148dfe5ed434ad7910f82a46ee6
AurelienChetot <aurelien.chetot@gmail.com>
2024-04-10 17:10:43 +0200

    Add Card

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,13 @@
+import "./App.css";
+import Card from "./components/Card";
+
 function App() {
-  return <h1>Notre Projet</h1>;
+  return (
+    <div>
+      <h1>Notre Projet</h1>
+      <Card />
+    </div>
+  );
 }
 
 export default App;
