f4046ab59bdf9563695de6ecda9e2d8c0f6ced46
Franck HERPOUX <franckherpoux@hotmail.fr>
2024-04-10 16:33:03 +0200

    Application du CSS sur le header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 function Header() {
   return (
     <header className="header">
-      <h1>Star Wild</h1>
+      <h1>Star-Wild:</h1>
+      <br />
+      <h2>L'odyssey de la wild</h2>
     </header>
   );
 }
