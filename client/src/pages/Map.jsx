fd2b9a3f1332dcaa6609993be26ace27b6d48348
Sachdarras <sachdarras@gmail.com>
2024-04-15 20:25:32 +0200

    display none sur la nav de app.jsx

--- client/src/pages/Map.jsx
+++ client/src/pages/Map.jsx
@@ -2,12 +2,14 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Canva from "../components/Canva";
 import Card from "../components/Card";
+import Nav from "../components/Nav";
 import "../App.css";
 
 function Map() {
   return (
     <div className="app-container">
       <Header />
+      <Nav />
       <Canva className="canva" />
       <Card />
       <Footer />
