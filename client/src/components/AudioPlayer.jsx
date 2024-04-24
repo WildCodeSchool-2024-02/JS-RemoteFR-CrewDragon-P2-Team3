828f4e218a0a20f52b1363192b151c6aae00b563
FabienLqr <159054866+FabienLqr@users.noreply.github.com>
2024-04-24 16:34:29 +0200

    Update AudioPlayer.jsx
    remove useless div

--- client/src/components/AudioPlayer.jsx
+++ client/src/components/AudioPlayer.jsx
@@ -3,14 +3,9 @@ import track from "../data/track";
 function AudioPlayer() {
   return (
     <div className="audio-player">
-      <div className="inner">
-        {" "}
-        <div>
           <audio src={track[0].src} controls>
             <track src={track[0].src} kind="captions" />
           </audio>
-        </div>
-      </div>
     </div>
   );
 }
