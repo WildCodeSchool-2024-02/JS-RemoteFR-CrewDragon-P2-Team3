e51664b103b5b26efe500ff6c04f507904c878f2
Sacha Darras <156203419+Sachdarras@users.noreply.github.com>
2024-04-25 14:55:41 +0200

    Update PlanetInfo.jsx
    saturne

--- client/src/components/PlanetInfo.jsx
+++ client/src/components/PlanetInfo.jsx
@@ -3,7 +3,7 @@ import venus from "../assets/bgMain/venusInfo.jpg";
 import terre from "../assets/bgMain/terreInfo.jpg";
 import mars from "../assets/bgMain/marsInfo.jpg";
 import jupiter from "../assets/bgMain/jupiterInfo.jpg";
-import saturn from "../assets/bgMain/saturneInfo.jpg";
+import saturne from "../assets/bgMain/saturneInfo.jpg";
 import uranus from "../assets/bgMain/uranusInfo.jpg";
 import neptune from "../assets/bgMain/neptuneInfo.jpg";
 
