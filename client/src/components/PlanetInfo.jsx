66c255a18c3e682415f3bd981ee20d9fb52683c1
FabienLqr <159054866+FabienLqr@users.noreply.github.com>
2024-04-25 14:53:10 +0200

    Update PlanetInfo.jsx

--- client/src/components/PlanetInfo.jsx
+++ client/src/components/PlanetInfo.jsx
@@ -4,7 +4,6 @@ import terre from "../assets/bgMain/terreInfo.jpg";
 import mars from "../assets/bgMain/marsInfo.jpg";
 import jupiter from "../assets/bgMain/jupiterInfo.jpg";
 import saturn from "../assets/bgMain/saturneInfo.jpg";
-
 import uranus from "../assets/bgMain/uranusInfo.jpg";
 import neptune from "../assets/bgMain/neptuneInfo.jpg";
 
