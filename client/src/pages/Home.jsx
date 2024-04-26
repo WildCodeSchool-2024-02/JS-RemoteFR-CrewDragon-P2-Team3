bb87bddb56b131cd4dbb0b4caf11952ab4b01070
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-04-26 10:06:07 +0200

    la musique ne se reset pas entre chaque page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,4 @@
 import Footer from "../components/Footer";
-import Header from "../components/Header";
-import Nav from "../components/Nav";
 import Section1 from "../components/Section1";
 import Tableau from "../components/Tableau";
 import Section2 from "../components/Section2";
@@ -9,8 +7,6 @@ import PlanetInfo from "../components/PlanetInfo";
 function Home() {
   return (
     <div className="app-container">
-      <Header />
-      <Nav />
       <Section1 />
       <PlanetInfo />
       <Section2 />
