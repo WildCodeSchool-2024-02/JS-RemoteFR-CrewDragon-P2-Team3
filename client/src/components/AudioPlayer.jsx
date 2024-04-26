8a4f80bdf1f8a9b7f839923341eeb563a2d94e90
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-04-26 10:57:44 +0200

    Et la musique se lance par defaut toute seule en plus de se repeat, et ca, ca c'est genial!

--- client/src/components/AudioPlayer.jsx
+++ client/src/components/AudioPlayer.jsx
@@ -3,7 +3,7 @@ import track from "../data/track";
 function AudioPlayer() {
   return (
     <div className="audio-player">
-      <audio src={track[0].src} controls loop>
+      <audio src={track[0].src} controls autoPlay loop>
         <track src={track[0].src} kind="captions" />
       </audio>
     </div>
